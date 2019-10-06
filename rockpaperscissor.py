2953340c5335e0b0269e3e1b06e3a222eb168ae8
Anjaneya varma <you@example.com>
2019-10-06 23:28:07 +0530

    second commit

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -7,19 +7,18 @@ print("lets play a game of Rock paper scissor")
 while True:
     print("enter choice \n 1.>Rock \n 2.>Paper \n 3> Scissor")
     #user input
-    choice = input("enter your guessed")
-    #build a condition to lo0p is option is incorrect
-    while choice < 1 or choice >3:
-        choice = int(input("invald option"))
-        #getting choice choice value
-        if choice ==1:
+    choice = int(input("enter your guessed"))
+    #build a condition to loop is option is incorrect
+    if(choice > 1 or choice < 4):
+        #getting choice value
+        if choice == 1:
             choice_name='Rock'
-        elif choice ==2:
+        elif choice == 2:
             choice_name='Paper'
         else:
             choice_name='Scissor'
         #printing user choice
-        print("user choice is :"+choice_name)
+        print("user choice is :" +choice_name)
         print("computer turn")
         #computer chooses randomly by ising randint method
         comp_choice =random.randint(1,3)
@@ -30,17 +29,17 @@ while True:
             comp_choice_name = 'Paper'
         else:
             comp_choice_name = 'Scissor'
-            print("computer name is :" + comp_choice_name)
+            print("computer name is :" +comp_choice_name)
 
-        #condition for game 
-        if ((choice==1 and comp_choice==2)or (choice==2 and comp_choice==1)):
+        #condition for game
+        if((choice==1 and comp_choice==2) or (choice==2 and comp_choice==1)):
             result = "Paper"
-        elif ((choice==2 and comp_choice==3)or (choice=3 and comp_choice==2)):
+        elif((choice==2 and comp_choice==3) or (choice==3 and comp_choice==2)):
             result ="Scissor"
-            else:
-                result = "Rock"
-            #print winner
-            if(result ==choice_name):
-                print("user is the winner")
-            else:
-                print("computer wins")
+        else:
+            result = "Rock"
+        #print winner
+        if(result ==choice_name):
+            print("user is the winner")
+        else:
+            print("computer wins")
